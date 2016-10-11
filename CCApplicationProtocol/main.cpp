b58fcf7de91a06e7b6dd3ec1770698eb0cc2b6c0
jack <jack@jackdeair.lan>
2016-10-12 01:00:25 +0800

    Initial Commit

---
+++ CCApplicationProtocol/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CCApplicationProtocol
+//
+//  Created by jack on 16/10/12.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
